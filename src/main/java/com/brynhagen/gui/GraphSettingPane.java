f28b0701ab34de04853042280c9b10a13a72dca7
Jonathan Brynhagen <jonathan.brynhagen@flygp.se>
2017-05-26 16:16:49 +0200

    More GUI stuff

--- src/main/java/com/brynhagen/gui/GraphSettingPane.java
+++ src/main/java/com/brynhagen/gui/GraphSettingPane.java
@@ -1,10 +1,17 @@
 package com.brynhagen.gui;
 
+import java.awt.Dimension;
+
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import net.miginfocom.swing.MigLayout;
+
+import com.brynhagen.entity.PlayerClass;
+import com.brynhagen.entity.Specialisation;
 
 /**
  * Created by jonathan on 26/05/17.
@@ -22,10 +29,17 @@ public class GraphSettingPane
 
 	private void init()
 	{
+		JComboBox<PlayerClass> playerClassJComboBox = new JComboBox();
+		playerClassJComboBox.setModel(new DefaultComboBoxModel<PlayerClass>(PlayerClass.values()));
+		playerClassJComboBox.setPreferredSize(new Dimension(40, 15));
 		add(new JComboBox<Object>(), "w 40");
-		add(new JButton("Encounter"), "w 40");
-		add(new JButton("Player"), "w 40");
-		add(new JButton("Class"), "w 40");
-		add(new JButton("Spec"), "w 40");
+		add(new JComboBox<Object>(), "w 40");
+		add(new JTextField("Player"), "w 40");
+		add(new JTextField("Realm"), "w 40");
+		add(playerClassJComboBox, "w 40!");
+
+		JComboBox<Specialisation> playerSpecJComboBox = new JComboBox();
+		playerSpecJComboBox.setModel(new DefaultComboBoxModel<Specialisation>(Specialisation.values()));
+		add(playerSpecJComboBox, "w 40");
 	}
 }
